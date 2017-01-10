be52f0ec152decbf5942dc80e0845b7098eab4d5
skarthick <sunil@knetworks.co.in>
2017-01-10 16:16:30 +0530

    Framework update

--- src/ios/riws.m
+++ src/ios/riws.m
@@ -18,6 +18,8 @@
 @property (nonatomic, assign) double heading;
 @property (nonatomic, assign) BOOL isProcessing;
 @property (nonatomic, retain) NSString* lastRemovedID;
+@property (nonatomic, retain) NSString* lastShownID;
+@property (nonatomic, assign) BOOL isLastOnRunway;
 
 -(void)addPolygon:(CDVInvokedUrlCommand*)command;
 -(void)removePolygon:(CDVInvokedUrlCommand*)command;
@@ -94,15 +96,85 @@ double RadiansToDegrees(double radians) {return radians * 180/M_PI;};
     [[RIWS sharedManager]initializes];
     [self initializes];
     
+    //    [NSTimer scheduledTimerWithTimeInterval:5.0
+    //                                     target:self
+    //                                   selector:@selector(simulateS2N)
+    //                                   userInfo:nil
+    //                                    repeats:NO];
+    
+    [NSTimer scheduledTimerWithTimeInterval:1.0
+                                     target:self
+                                   selector:@selector(refresh)
+                                   userInfo:nil
+                                    repeats:YES];
+    
 }
-
+-(void)simulateN2S{
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void)
+                   {
+                       NSString *n2s = @"-77.42150244187999,39.0228742919202,0 -77.42160484620246,39.02286974297433,0 -77.42163045178441,39.02286861030525,0 -77.42167128650239,39.02286680752272,0 -77.42172577689561,39.02286440268998,0 -77.42177441809552,39.02286225168715,0 -77.42181744387666,39.02286188727877,0 -77.42188240628222,39.02285895533722,0 -77.42193394321212,39.02285662762361,0 -77.42198244857924,39.02285725277748,0 -77.4220365027393,39.02285472900995,0 -77.42210824924604,39.02285137438871,0 -77.42218808824009,39.0228451588434,0 -77.42222631547716,39.02284343162182,0 -77.42226028845938,39.02284190041315,0 -77.42229674760891,39.02284025685182,0 -77.42231627922803,39.02283937568538,0 -77.42235711831155,39.02283422403918,0 -77.42238249173364,39.02283530263501,0 -77.42241659336298,39.02283167846699,0 -77.4224617339413,39.02282875259296,0 -77.42248258605186,39.02282587642751,0 -77.42249139371371,39.02282552325049,0 ";
+                       
+                       NSArray* tArr = [n2s componentsSeparatedByString:@" "];
+                       for (int i =0; i < [tArr count]; i++) {
+                           NSArray *ttArr= [[tArr objectAtIndex:i]componentsSeparatedByString:@","];
+                           //        if (i==8) {
+                           //            break;
+                           //        }
+                           if ([ttArr count]<2) {
+                               continue;
+                           }
+                           NSString *tlongi = [ttArr objectAtIndex:0];
+                           NSString *tLati = [ttArr objectAtIndex:1];
+                           [[RIWS sharedManager]checkPointinPolygonLatitude:[tLati doubleValue] Longitude:[tlongi doubleValue] Speed:10 Heading:10];
+                           [NSThread sleepForTimeInterval:1.0f];
+                       }
+                       
+                       [NSTimer scheduledTimerWithTimeInterval:10.0
+                                                        target:self
+                                                      selector:@selector(simulateS2N)
+                                                      userInfo:nil
+                                                       repeats:NO];
+                   });
+}
+-(void)simulateS2N{
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void)
+                   {
+                       NSString *n2s = @"-77.42257052812694,39.02284480198515,0 -77.42254558479432,39.02284244506717,0 -77.42252119416209,39.02284380230371,0 -77.42249286089645,39.02284438116254,0 -77.42246610332147,39.02284483688361,0 -77.42242412031267,39.02284710599888,0 -77.42240267125928,39.02284827181609,0 -77.42235821450147,39.02285072320451,0 -77.4223151906977,39.02285307943477,0 -77.42227974196446,39.02285618574153,0 -77.42220286074063,39.02286045870276,0 -77.4221490315704,39.02286345802722,0 -77.42209604094924,39.02285983128024,0 -77.42202081922059,39.02286783686071,0 -77.42193838570988,39.02287228138474,0 -77.42183032274932,39.02288423156463,0 -77.42174025770582,39.02289096785093,0 -77.42164994549609,39.02290120903151,0 -77.42158635942157,39.02291021661701,0 ";
+                       NSArray* tArr = [n2s componentsSeparatedByString:@" "];
+                       for (int i =0; i < [tArr count]; i++) {
+                           NSArray *ttArr= [[tArr objectAtIndex:i]componentsSeparatedByString:@","];
+                           if ([ttArr count]<2) {
+                               continue;
+                           }
+                           //       if (i==9) {
+                           //           break;
+                           //        }
+                           NSString *tlongi = [ttArr objectAtIndex:0];
+                           NSString *tLati = [ttArr objectAtIndex:1];
+                           [[RIWS sharedManager]checkPointinPolygonLatitude:[tLati doubleValue] Longitude:[tlongi doubleValue] Speed:10 Heading:10];
+                           [NSThread sleepForTimeInterval:1.0f];
+                       }
+                       
+                       //    [NSTimer scheduledTimerWithTimeInterval:10.0
+                       //                                     target:self
+                       //                                   selector:@selector(simulateN2S)
+                       //                                   userInfo:nil
+                       //                                    repeats:NO];
+                   });
+}
 -(void)initializes{
     [[SimpleBadElfGpsManagers sharedGpsManager] setDelegate:self];
     [[SimpleBadElfGpsManagers sharedGpsManager] setAutoOpenAccessories:YES];
     [[SimpleBadElfGpsManagers sharedGpsManager] start];
     self.gpsDataFormatter = [[GpsDataFormatters alloc] init];
     [self startLocationManagerUpdates];
-    [self refresh];
+    //    [self refresh];
+    //    NSDictionary *incrusion = @{
+    //                                @"IncursionEventID" : @"Started initially"
+    //                                };
+    //    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:incrusion];
+    //    [pluginResult setKeepCallbackAsBool:TRUE];
+    //    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.eventCommand.callbackId];
 }
 
 #pragma mark - CoreLocation Methods
@@ -197,7 +269,7 @@ double RadiansToDegrees(double radians) {return radians * 180/M_PI;};
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
-    [self refresh];
+    //    [self refresh];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
@@ -248,39 +320,40 @@ double RadiansToDegrees(double radians) {return radians * 180/M_PI;};
  */
 
 - (void)gpsAccessory:(id<BEGpsAccessory>)accessory locationUpdated:(id<BEGpsLocation>)location {
-    [self refresh];
+    //    [self refresh];
 }
 
 - (void)gpsAccessory:(id<BEGpsAccessory>)accessory satellitesUpdated:(NSArray*)satellites {
-    [self refresh];
+    //    [self refresh];
 }
 
 - (void)gpsAccessory:(id<BEGpsAccessory>)accessory asciiUpdated:(NSString*)ascii {
-    [self refresh];
+    //    [self refresh];
 }
 
 - (void)gpsAccessoryStatusUpdated:(id<BEGpsAccessory>)accessory {
-    [self refresh];
+    //    [self refresh];
 }
 
 - (void)gpsAccessoryConnected:(id<BEGpsAccessory>)accessory {
     
     //[self.badElfGpsManager openAccessory:accessory]; // only needed if autoOpen not enabled on BadElfGpsManager
-    [self refresh];
+    //    [self refresh];
 }
 - (void) gpsAccessoryDisconnected:(id<BEGpsAccessory>)accessory {
     
-    [self refresh];
+    //    [self refresh];
 }
 
 
 - (void) refresh {
-    if (self.isProcessing) {
-        return;
-    }
-    self.isProcessing = TRUE;
+    
     dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void)
                    {
+                       if (self.isProcessing) {
+                           return;
+                       }
+                       self.isProcessing = TRUE;
                        id<BEGpsAccessory> gps = [[SimpleBadElfGpsManagers sharedGpsManager] selectedHardware];
                        id <BEGpsLocation> location = gps.currentLocation;
                        if (gps) {
@@ -301,39 +374,72 @@ double RadiansToDegrees(double radians) {return radians * 180/M_PI;};
 
 #pragma mark RIWS Delegates
 -(void)RunwayIncrusionOccurredAtRunway:(NSString *)runwayName RunwayID:(NSString *)runwayID isTargetOnRunway:(BOOL)onRunway{
-    
+    BOOL toSend = FALSE;
+    if (![self.lastShownID isEqualToString:runwayID]) {
+        toSend = TRUE;
+        self.lastShownID = runwayID;
+        self.isLastOnRunway = onRunway;
+    }else{
+        if (self.isLastOnRunway != onRunway) {
+            toSend = TRUE;
+            self.isLastOnRunway = onRunway;
+        }
+    }
+    if (!toSend) {
+        return;
+    }
+    self.lastRemovedID = @"";
     NSString *textColor = @"e9f612";
     NSString *message = [NSString stringWithFormat:@"Vehicle is predicted to hit %@",runwayName];
     if (onRunway) {
         textColor = @"b20707";
         message = [NSString stringWithFormat:@"Vehicle is inside %@",runwayName];
     }
-    
-    NSDictionary *incrusion = @{
-                                @"IncursionEventID" : runwayID,
-                                @"IncursionText" : message,
-                                @"TextColor" : textColor,
-                                @"AudioFile" : @"1.mp3"
-                                };
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:incrusion];
-    [pluginResult setKeepCallbackAsBool:TRUE];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.eventCommand.callbackId];
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void)
+                   {
+                       
+                       NSDictionary *incrusion = @{
+                                                   @"IncursionEventID" : runwayID,
+                                                   @"IncursionText" : message,
+                                                   @"TextColor" : textColor,
+                                                   @"AudioFile" : @"1.mp3",
+                                                   @"Time" : [NSString stringWithFormat:@"%.0f", [[NSDate date] timeIntervalSince1970]]
+                                                   };
+                       NSLog(@"Found : %@",incrusion);
+                       //    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:incrusion];
+                       CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%@",incrusion]];
+                       [pluginResult setKeepCallbackAsBool:TRUE];
+                       [self.commandDelegate sendPluginResult:pluginResult callbackId:self.eventCommand.callbackId ];
+                   });
 }
 
 -(void)RunwayIncrusionRemovededFromRunway:(NSString *)runwayName RunwayID:(NSString *)runwayID{
+    
     if (!runwayID) {
         runwayID = @"Started initially";
     }
+    self.lastShownID = @"";
+    self.isLastOnRunway = false;
+    //    dispatch_async(dispatch_get_main_queue(), ^(void)
+    //                   {
     if ( [self.lastRemovedID isEqualToString:runwayID]) {
         return;
     }
     self.lastRemovedID = runwayID;
     NSDictionary *incrusion = @{
-                                @"IncursionEventID" : runwayID
+                                @"IncursionEventID" : runwayID,
+                                @"Time" : [NSString stringWithFormat:@"%.0f", [[NSDate date] timeIntervalSince1970]]
                                 };
+    //    NSDictionary *incrusion = @{
+    //                                @"IncursionEventID" : runwayID,
+    //                                @"IncursionText" : @"ttttt ttttttt",
+    //                                @"TextColor" : @"67y8hb",
+    //                                @"AudioFile" : @"1.mp3"
+    //                                };
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:incrusion];
     [pluginResult setKeepCallbackAsBool:TRUE];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.eventCommand.callbackId];
+    //                   });
 }
 
 #pragma mark init predefined polygons
