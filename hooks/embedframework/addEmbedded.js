e55008a2a9c20ab0d56ff3549b083d85c7ce9731
vishknetworks <vish@knetworks.co.in>
2017-01-17 17:05:36 -0500

    Removed the leading quotes

--- hooks/embedframework/addEmbedded.js
+++ hooks/embedframework/addEmbedded.js
@@ -90,7 +90,7 @@ module.exports = function(context) {
     // unquote (remove trailing ")
     //var projectName = myProj.getFirstTarget().firstTarget.name.substr(1);
     var projectName = myProj.getFirstTarget().firstTarget.name;
-    projectName = projectName.substr(0, projectName.length-1); //Removing the char " at beginning and the end.
+    projectName = projectName.substr(1, projectName.length-2); //Removing the char " at beginning and the end.
 
     const groupName = 'Embed Frameworks ' + context.opts.plugin.id;
     const pluginPathInPlatformIosDir = projectName + '/Plugins/' + context.opts.plugin.id;
